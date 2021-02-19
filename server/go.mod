aa29c55156e8548a1a1e7b00e1e059f48f28132e
Alireza <s.alireza.mohammadi@gmail.com>
2021-02-19 18:54:19 +0000

    add go server

---
+++ server/go.mod
@@ -0,0 +1,9 @@
+module github.com/salirezam/grpc-web
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.4.3 // indirect
+	google.golang.org/grpc v1.35.0 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
---
+++ server/server.go
@@ -0,0 +1,39 @@
+package main
+
+import(
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/salirezam/grpc-web/messagepb"
+	"google.golang.org/grpc"
+)
+
+type server struct{
+	messagepb.UnimplementedMessageServer
+}
+
+func (*server) SayHello(context context.Context, req *messagepb.MesaageRequest) (*messagepb.MessageResponse, error) {
+	fmt.Println("Got a new Add request")
+	message := req.GetMessage()
+	helloMessage := fmt.Sprintf("Hello %s", message)
+	result := &messagepb.MessageResponse{Result: helloMessage}
+	return result, nil
+}
+
+func main() {
+	fmt.Println("Starting Calculator server")
+	lis, err := net.Listen("tcp", "0.0.0.0:50888")
+
+	if err != nil {
+		log.Fatalf("Error while listening : %v", err)
+	}
+ 
+	s := grpc.NewServer()
+	messagepb.RegisterMessageServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Error while serving : %v", err)
+	}
+
+}
